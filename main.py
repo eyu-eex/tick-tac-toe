ed91561bf3bbcf7e10664211b9d63264f2d49a47
eyu-eex <eyuelseyoum92@gmail.com>
2022-05-19 08:37:03 +0300

    Refactor replay function

--- main.py
+++ main.py
@@ -42,85 +42,91 @@ def update_board(game_board,user_choice ,player):
 
     return game_board
 
-def check_winner(game_list):
+def check_winner(board):
     """Checks whether all postions are filled in the board in which case its a tie. Otherwise, Checks if the cells are empty then checks if they are the same.
     """
-    if all(game_list):
+    if all(board):
         print("TIE!")
         return True
 
-    if game_list[0] and game_list[1] and game_list[2]:
-        if game_list[0] == game_list[1] == game_list[2]:
+    if board[0] and board[1] and board[2]:
+        if board[0] == board[1] == board[2]:
             print("WINNER!")
             return True
 
-    elif game_list[3] and game_list[4] and game_list[5]:
-        if game_list[3] == game_list[4] == game_list[5]:
+    elif board[3] and board[4] and board[5]:
+        if board[3] == board[4] == board[5]:
             print("WINNER!")
             return True
     
-    elif game_list[6] and game_list[7] and game_list[8]:
-        if game_list[6] == game_list[7] == game_list[8]:
+    elif board[6] and board[7] and board[8]:
+        if board[6] == board[7] == board[8]:
             print("WINNER!")
             return True
 
-    elif game_list[0] and game_list[3] and game_list[6]:
-        if game_list[0] == game_list[3] == game_list[6]:
+    elif board[0] and board[3] and board[6]:
+        if board[0] == board[3] == board[6]:
             print("WINNER!")
             return True
 
-    elif game_list[1] and game_list[4] and game_list[7]:
-        if game_list[1] == game_list[4] == game_list[7]:
+    elif board[1] and board[4] and board[7]:
+        if board[1] == board[4] == board[7]:
             print("WINNER!")
             return True
     
-    elif game_list[2] and game_list[5] and game_list[8]:
-        if game_list[2] == game_list[5] == game_list[8]:
+    elif board[2] and board[5] and board[8]:
+        if board[2] == board[5] == board[8]:
             print("WINNER!")
             return True
 
-    elif game_list[0] and game_list[4] and game_list[8]:
-        if game_list[0] == game_list[4] == game_list[8]:
+    elif board[0] and board[4] and board[8]:
+        if board[0] == board[4] == board[8]:
             print("WINNER!")
             return True
     
-    elif game_list[2] and game_list[4] and game_list[6]:
-        if game_list[2] == game_list[4] == game_list[6]:
+    elif board[2] and board[4] and board[6]:
+        if board[2] == board[4] == board[6]:
             print("WINNER!")
             return True
     
     return False
 
-def display(game_list):
+def display(board):
     print('\n'*100)
 
-    print(' ' + game_list[7] + ' | ' + game_list[8] + ' | ' + game_list[9])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 
     print('-----------')
 
-    print(' ' + game_list[4] + ' | ' + game_list[5] + ' | ' + game_list[6])
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
 
     print('-----------')
 
-    print(' ' + game_list[1] + ' | ' + game_list[2] + ' | ' + game_list[3])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
 
 
+def replay():
+    choice = ''
+    while choice in ['X', 'Y']:
+        choice = input('Do you want to play again? (Yor N): ').upper()
+
+    return choice == 'Y'
 def game():
-    game_list = [''] * 9
+    board = [''] * 9
     gameover= False
     player1 = 'X'
     player2 = 'O'
     while not gameover:
         choice = user_input()
-        game_list = update_board(game_list, choice, player1)
-        display(game_list)
-        gameover = check_winner(game_list)   
+        board = update_board(board, choice, player1)
+        display(board)
+        gameover = check_winner(board)   
         if gameover:
             break 
         choice = user_input()
-        game_list = update_board(game_list, choice, player2)
-        display(game_list)
-        gameover = check_winner(game_list)
+        board = update_board(board, choice, player2)
+        display(board)
+        gameover = check_winner(board)
     replay = input("Do you wwant to play again? (Y/N)? ")
     if replay == 'Y':
         game()
